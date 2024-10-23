186afe8cb82e6e0e60d434dee766d0d132560855
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-24 02:50:35 +0530

    New route /api/generate-question added

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,55 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { z, ZodError } from "zod";
+import { config } from "dotenv";
+
+config();
 
 const app = new Hono();
+const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 app.get("/", (c) => {
   return c.text("Hello World");
+});
+
+app.post("/api/generate-question", async (c) => {
+  try {
+    const validator = z.object({
+      topic: z.enum(
+        [
+          "JavaScript",
+          "Python",
+          "React.js",
+          "Next.js",
+          "React Native",
+          "Java",
+          "C",
+          "C++",
+        ],
+        { message: "Topic is invalid" }
+      ),
+    });
+    const data = await c.req.json();
+    const { topic } = await validator.parseAsync(data);
+
+    const res = await model.generateContent(
+      `Give me a MCQ question on the topic of ${topic}. The response should be in the form: {question: string; option1: string; option2: string; option3: string; option4: string; rightAnswer: "option1" | "option2" | "option3" | "option4"}. Only respond in the specified format's JSON-encoded string. Do not include anything else in the response. Double check the question and make sure there is no mistake.`
+    );
+    const question = JSON.parse(res.response.text());
+
+    c.status(200);
+    return c.json({ question });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      c.status(422);
+      return c.json({ error: error.errors[0].message });
+    } else {
+      c.status(500);
+      return c.json({ error: "Some error occured. Please try again later" });
+    }
+  }
 });
 
 console.log(`Running on http://localhost:3000`);
