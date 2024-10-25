a647ab1824f567a2b04c5629a9f0372fe03f2fee
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-25 20:47:01 +0530

    result page complete

--- client/app/result.tsx
+++ client/app/result.tsx
@@ -1,11 +1,17 @@
 import { View, Text, Pressable } from "react-native";
 import React from "react";
 import tw from "twrnc";
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 
 import ImageWrapper from "@/components/ImageWrapper";
+import { useNumberOfRightAnswers } from "@/hooks/useNumberOfRightAnswers";
 
 const Result = () => {
+  const { numberOfQuestions } = useLocalSearchParams() as {
+    numberOfQuestions: string;
+  };
+  const { numberOfRightAnswers, setNumberOfRightAnswers } =
+    useNumberOfRightAnswers();
   return (
     <ImageWrapper>
       <View style={tw`items-center gap-y-6 w-full`}>
@@ -15,16 +21,25 @@ const Result = () => {
         <View
           style={tw`bg-emerald-600 rounded-full w-28 h-28 items-center justify-center`}
         >
-          <Text style={tw`text-white text-4xl font-bold`}>60%</Text>
+          <Text style={tw`text-white text-4xl font-bold`}>
+            {Math.ceil(
+              (numberOfRightAnswers / parseInt(numberOfQuestions)) * 100
+            )}
+            %
+          </Text>
         </View>
 
         <Text style={tw`text-white text-base font-medium`}>
-          You correctly answered 2 questions out of 5
+          You correctly answered {numberOfRightAnswers} questions out of{" "}
+          {numberOfQuestions}
         </Text>
 
         <Pressable
           style={tw`bg-emerald-600 w-[90%] items-center justify-center py-3 rounded-xl`}
-          onPress={router.back}
+          onPress={() => {
+            setNumberOfRightAnswers(0);
+            router.back();
+          }}
         >
           <Text style={tw`text-white text-lg font-medium`}>Go back</Text>
         </Pressable>
